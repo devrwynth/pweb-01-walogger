a774ce0e094e2e667cb1b74bfc34da305e1c0c4b
Faisal Azmi Sirajuddin <162958780+faalas@users.noreply.github.com>
2024-11-11 20:16:33 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 
@@ -73,6 +73,3 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
